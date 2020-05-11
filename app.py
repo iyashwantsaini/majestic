069bd836483ea6fd210e944bd83fbdb4f8170de7
paras55 <52483274+paras55@users.noreply.github.com>
2020-05-12 01:01:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, redirect, url_for, flash, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
@@ -7,33 +7,18 @@ app = Flask(__name__)
 def index():
         return render_template("login.html")
 
-# @app.route("/register")
-# def register():
-#     return render_template("register.html")
 
-@app.route("/dashboard")
-def dashboard():
-    return render_template("dashboard.html")
 
-@app.route("/searchengine")
-def searchengine():
-    return render_template("searchengine.html")
-
-@app.route("/wordcloud")
-def wordcloud():
-    return render_template("wordcloud.html")
-
-@app.route("/summarization")
-def summarization():
-    return render_template("summarization.html")
-
-@app.route("/qna")
-def qna():
-    return render_template("qna.html")
-
-# @app.route("/profile")
-# def profile():
-#     return render_template("profile.html")
+@app.route('/selection',methods=['POST','GET'])
+def selection():
+    
+    username = request.form['username']
+    password= request.form['password']
+    
+    if username=='thapar' and password=='thapar':
+        return render_template('dashboard.html')
+    else :
+        return render_template('login.html', warning='Please enter correct username and password')
 
 if __name__ == "__main__":
-    app.run(debug=True,use_reloader=True)
+    app.run()
