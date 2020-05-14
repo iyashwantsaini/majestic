1ee568235ad9248f43ce1c34a617e183c37125f4
paras55 <52483274+paras55@users.noreply.github.com>
2020-05-15 00:40:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 import os
+import arxiv
+import pandas as pd
 from flask import Flask, redirect, url_for, flash, render_template,request
+import requests
+import pandas as pd
+import math
+
+titles_list = []
+links_list = []
+date_list = []
+doi_list=[]
+citation_list=[]
+abstract_list=[]
+author_list=[]
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+        
         return render_template("login.html")
 
 @app.route('/selection',methods=['POST'])
@@ -22,6 +36,66 @@ def selection():
 @app.route("/searchengine")
 def searchengine():
     return render_template("searchengine.html")
+
+@app.route("/found",methods=['POST','GET'])
+def found():
+    branch = request.form['engine'] #ieee or arxiv
+    keyword = request.form['keyword']
+    noofresults = request.form['number']
+    noofresults = int(noofresults)
+    print('hello')
+    branch=branch.lower()
+    if branch=='arxiv':
+        print('hi')
+        result = arxiv.query(query=keyword,max_results=noofresults)
+        data = pd.DataFrame(columns = ["Title",'Published Date','Download Link'])
+        for i in range(len(result)):
+          title = result[i]['title']
+          arxiv_url = result[i]['arxiv_url']
+          arxiv_url=arxiv_url.replace('abs','pdf')
+          published = result[i]['published']
+          data_tmp = pd.DataFrame({"Title":title, "Published Date":published, "Download Link":arxiv_url},index=[0])
+          data = pd.concat([data,data_tmp]).reset_index(drop=True) #dataframe
+        return render_template('searchengine.html',tables=[data.to_html(render_links=True,classes=['table table-bordered'])]);
+    elif branch=='ieee':
+        page_no = 1
+        no = math.ceil(noofresults/25)
+        for page_no in range(1, no+1):
+
+            headers = {
+                "Accept": "application/json, text/plain, */*",
+                "Origin": "https://ieeexplore.ieee.org",
+                "Content-Type": "application/json",
+            }
+            payload = {
+                "newsearch": True,
+                "queryText": keyword,
+                "highlight": True,
+                "returnFacets": ["ALL"],
+                "returnType": "SEARCH",
+                "pageNumber": page_no
+            }
+            r = requests.post(
+                    "https://ieeexplore.ieee.org/rest/search",
+                    json=payload,
+                    headers=headers
+                )
+            page_data = r.json()
+            for record in page_data["records"]:
+                titles_list.append(record["articleTitle"])
+                links_list.append('https://ieeexplore.ieee.org'+record["documentLink"])
+                date_list.append(record["publicationDate"])
+                citation_list.append(record["citationCount"])
+                #author_list.append(record["authors"])
+                '''if record["doi"] == '' or None :
+                    doi_list.append("none")
+                else:
+                    doi_list.append(record["doi"])'''
+        
+            d = {"Title": titles_list, "Link": links_list, "Publication Date": date_list,  "No of Citations" : citation_list }
+            df = pd.DataFrame.from_dict(d)
+            finaldf = df[:noofresults] #dataframe
+        
 
 @app.route("/dashboard")
 def dashboard():
@@ -56,4 +130,4 @@ def profile():
     return render_template("profile.html")
 
 if __name__ == "__main__":
-    app.run(debug=True,use_reloader=True)
+    app.run()
