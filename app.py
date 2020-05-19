75b92069463d76735bebfbac8e689b87b0c7b52c
paras55 <52483274+paras55@users.noreply.github.com>
2020-05-19 17:12:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -226,15 +226,21 @@ def uploader():
         body_without_tag = body.replace("<p>", "").replace("</p>", "").replace("<div>", "").replace("</div>","").replace("<p />","")
         text_pages = body_without_tag.split("""<div class="page">""")[1:]
         num_pages = len(text_pages)
-        new = open("PDF/"+str(name)+'_'+pa,"w")
+        new = open("static/pdf/"+str(name)+'_'+pa,"w",encoding="utf-8")
         #print(num_pages)
         if num_pages==int(raw_xml['metadata']['xmpTPg:NPages']) : #check if it worked correctly
          for i in range(num_pages):
            new.write(text_pages[i])
            new.write(" \n page_ended \n ") 
-        os.remove(str(fp))
-        filename = secure_filename(ok.filename)
-        ok.save(os.path.join("PDF/",str(name)+'_'+filename))
+        no=os.getcwd()
+        plis=no+'/'+str(fp)
+        filedir = "static/pdf/"
+        shutil.move(plis,filedir)
+        ren_src="static/pdf/"+str(fp)
+        ren_des="static/pdf/"+current_user.username+'_'+str(fp)
+        os.rename(ren_src,ren_des)
+        
+        
         return render_template('dashboard.html',current_user=current_user)
 
 @app.route("/upload")
